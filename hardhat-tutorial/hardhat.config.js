82a3718f400f01475db5882ef07579e126b27447
tnsqjahong <cruseight47@naver.com>
2022-07-14 02:07:23 +0900

    first commit

---
+++ hardhat-tutorial/hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config({ path: ".env" });
+require("@nomicfoundation/hardhat-toolbox");
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
+
+const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY;
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [MUMBAI_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: POLYGONSCAN_KEY,
+    },
+  },
+};
